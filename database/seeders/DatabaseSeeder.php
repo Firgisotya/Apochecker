62fae716ccf145b2e6e10fbe256c6b2478ce2734
Shankara21 <lazuardit21@gmail.com>
2022-03-23 20:42:23 +0700

    Membuat Rancangan Awal Layouts

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('users')->insert([
+            [
+                'name' => 'Muhammad Lazuardi Timur',
+                'email' => 'lazuardit21@gmail.com',
+                'password' => Hash::make('123'),
+            ],
+            [
+                'name' => 'Firgi Sotya Izzuddin',
+                'email' => 'firgisotya@gmail.com',
+                'password' => Hash::make('123'),
+            ]
+        ]);
     }
 }
