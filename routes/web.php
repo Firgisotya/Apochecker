2d079ff1e86d90e87a6b812335440a4f8396cc2c
Firgisotya <zzuuddiinn28@gmail.com>
2022-03-23 23:11:13 +0700

    Edit Route

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,17 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', HomeController::class)->name('home');
+
+Route::middleware(['auth', 'isAdmin'])->group(function(){
+    Route::get('/dashboard', function(){
+        return view('admin.index');
+    })->name('index');
+
+});
