35ac57030ef420041f37f5ead7cbe5e11b770555
Firgisotya <zzuuddiinn28@gmail.com>
2022-03-23 23:35:30 +0700

    update hari ini

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,24 +15,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-<<<<<<< HEAD
+
         // \App\Models\User::factory(10)->create();
 
         $this->call([
             UserSeeder::class,
-=======
-        DB::table('users')->insert([
-            [
-                'name' => 'Muhammad Lazuardi Timur',
-                'email' => 'lazuardit21@gmail.com',
-                'password' => Hash::make('123'),
-            ],
-            [
-                'name' => 'Firgi Sotya Izzuddin',
-                'email' => 'firgisotya@gmail.com',
-                'password' => Hash::make('123'),
-            ]
->>>>>>> 62fae716ccf145b2e6e10fbe256c6b2478ce2734
+
         ]);
     }
 }
